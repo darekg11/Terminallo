e6c2266f5abee64747374fc6cbb304184cf86566
Dariusz Gorak <dariusz.gorak@rspective.pl>
2018-01-21 20:05:08 +0100

    Initial commit with npm configuration

---
+++ webpack.dev.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/app/App.js'],
+  target: 'electron',
+
+  output: {
+    path: path.join(__dirname, '/public/built'),
+    filename: 'bundle.js',
+    publicPath: '/built/',
+  },
+
+  devServer: {
+    port: 12000,
+    contentBase: './public',
+    publicPath: '/built/',
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+};
---
+++ webpack.prod.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJs = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/app/App.js'],
+  target: 'electron',
+
+  output: {
+    path: path.join(__dirname, '/public/built'),
+    filename: 'bundle.js',
+    publicPath: '/built/',
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new UglifyJs()
+  ],
+};
