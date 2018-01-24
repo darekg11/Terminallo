74cc599bcb977c0a52f5e53b435e9cdb01c7106a
Dariusz Gorak <dariusz.gorak@rspective.pl>
2018-01-24 23:59:33 +0100

    Basic virtual terminal is now fully working

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -25,5 +25,5 @@ module.exports = {
       { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
     ],
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.ExternalsPlugin('commonjs', ['node-pty'])],
 };
