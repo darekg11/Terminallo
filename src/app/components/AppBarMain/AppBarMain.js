ba7cf1efac54375a6436a754569e3071fa8cb821
Dariusz Gorak <dariusz.gorak@rspective.pl>
2018-02-03 20:26:44 +0100

    Added Save and Save as terminals icon buttons to AppBar

--- src/app/components/AppBarMain/AppBarMain.js
+++ src/app/components/AppBarMain/AppBarMain.js
@@ -5,6 +5,8 @@ import SettingsIcon from 'material-ui-icons/Settings';
 import AddIcon from 'material-ui-icons/Add';
 import ReorderIcon from 'material-ui-icons/Reorder';
 import IconButton from 'material-ui/IconButton';
+import SaveIcon from 'material-ui-icons/Save';
+import SaveAsIcon from 'material-ui-icons/ContentCopy';
 import Tooltip from 'material-ui/Tooltip';
 import './AppBarMain.css';
 
@@ -21,6 +23,21 @@ function AppBarMain() {
           <Tooltip id="tooltip-add-new" title="Add terminal">
             <IconButton aria-label="Add terminal">
               <AddIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip id="tooltip-add-new" title="Add terminal">
+            <IconButton aria-label="Add terminal">
+              <AddIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip id="tooltip-save" title="Save terminals">
+            <IconButton aria-label="Save terminals">
+              <SaveIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip id="tooltip-save-as" title="Save terminals as">
+            <IconButton aria-label="Save terminals as">
+              <SaveAsIcon />
             </IconButton>
           </Tooltip>
           <Tooltip id="tooltip-options" title="Options">
