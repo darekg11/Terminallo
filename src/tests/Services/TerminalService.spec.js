ad1edb051b82e4c55e1d6e5e76e6c404708c6119
Dariusz Gorak <dariusz.gorak@rspective.pl>
2018-02-25 15:11:04 +0100

    Added unit tests for FileService and missing tests for TerminalService

--- src/tests/Services/TerminalService.spec.js
+++ src/tests/Services/TerminalService.spec.js
@@ -1,8 +1,18 @@
 import { expect } from 'chai';
 import 'chai/register-should';
+import sinon from 'sinon';
 import * as TerminalService from '../../app/services/TerminalService';
 
+let sinonSandbox = null;
+
 describe('Terminal service', () => {
+  beforeEach(() => {
+    sinonSandbox = sinon.sandbox.create();
+  });
+  afterEach(() => {
+    sinonSandbox.restore();
+  });
+
   describe('exportTermninalsToObject', () => {
     it('should return empty object for empty input', () => {
       const result = TerminalService.exportTermninalsToObject([]);
@@ -63,6 +73,96 @@ describe('Terminal service', () => {
       expect(result.terminals[0].name).to.be.equal('Terminal');
       expect(result.terminals[0].terminalStartupDir).to.be.equal('../someDir/dir1');
       expect(result.terminals[0].terminalStartupCommands.length).to.be.equal(0);
+    });
+  });
+
+  describe('importTerminalsToObject method', () => {
+    it('should throw error when jsonFile does not exist', () => {
+      expect(() => TerminalService.importTerminalsToObject()).to.throw('Selected file is not valid for terminals import');
+    });
+
+    it('should throw error when jsonFile is missing terminals property', () => {
+      expect(() => TerminalService.importTerminalsToObject({})).to.throw('Selected file is not valid for terminals import');
+    });
+
+    it('should return empty array when terminals property is an empty array', () => {
+      expect(TerminalService.importTerminalsToObject({ terminals: [] })).to.deep.equal({ terminals: [] });
+    });
+
+    it('should return converted terminals', () => {
+      const testInput = {
+        terminals: [
+          {
+            uuid: 'test1234',
+            terminalType: 'someTerminalType',
+            name: 'Terminal1',
+            terminalStartupDir: 'SomePath',
+            terminalStartupCommands: ['command1', 'command2'],
+          },
+          {
+            uuid: 'test5678',
+            terminalType: 'anotherTerminalType',
+            name: 'Terminal2',
+            terminalStartupDir: 'SomeDifferentPath',
+            terminalStartupCommands: ['cmd1', 'cmd2'],
+          },
+        ],
+      };
+      const result = TerminalService.importTerminalsToObject(testInput);
+      expect(result.terminals[0].uuid).to.not.be.equal('test1234');
+      expect(result.terminals[0].terminalType).to.be.equal('someTerminalType');
+      expect(result.terminals[0].terminalName).to.be.equal('Terminal1');
+      expect(result.terminals[0].terminalStartupDir).to.be.equal('SomePath');
+      expect(result.terminals[0].terminalStartupCommands[0]).to.be.equal('command1');
+      expect(result.terminals[0].terminalStartupCommands[1]).to.be.equal('command2');
+
+      expect(result.terminals[1].uuid).to.not.be.equal('test5678');
+      expect(result.terminals[1].terminalType).to.be.equal('anotherTerminalType');
+      expect(result.terminals[1].terminalName).to.be.equal('Terminal2');
+      expect(result.terminals[1].terminalStartupDir).to.be.equal('SomeDifferentPath');
+      expect(result.terminals[1].terminalStartupCommands[0]).to.be.equal('cmd1');
+      expect(result.terminals[1].terminalStartupCommands[1]).to.be.equal('cmd2');
+    });
+
+    it('should return fallback default value for terminals', () => {
+      const testInput = {
+        terminals: [
+          {
+            terminalType: 'someTerminalType',
+          },
+          {
+            terminalType: 'anotherTerminalType',
+          },
+        ],
+      };
+      const result = TerminalService.importTerminalsToObject(testInput);
+      expect(result.terminals[0].uuid).to.not.be.equal('');
+      expect(result.terminals[0].terminalType).to.be.equal('someTerminalType');
+      expect(result.terminals[0].terminalName).to.be.equal('Terminal');
+      expect(result.terminals[0].terminalStartupDir).to.be.equal('');
+      expect(result.terminals[0].terminalStartupCommands.length).to.be.equal(0);
+
+      expect(result.terminals[1].uuid).to.not.be.equal('');
+      expect(result.terminals[1].terminalType).to.be.equal('anotherTerminalType');
+      expect(result.terminals[1].terminalName).to.be.equal('Terminal');
+      expect(result.terminals[1].terminalStartupDir).to.be.equal('');
+      expect(result.terminals[1].terminalStartupCommands.length).to.be.equal(0);
+    });
+  });
+
+  describe('killTerminalInstance method', () => {
+    it('should call closing method on terminal instance', () => {
+      const terminalInstance = {
+        xTermInstance: {
+          destroy: sinon.spy(),
+        },
+        virtualTerminalInstance: {
+          kill: sinon.spy(),
+        },
+      };
+      TerminalService.killTerminalInstance(terminalInstance);
+      expect(terminalInstance.xTermInstance.destroy.calledOnce).to.be.equal(true);
+      expect(terminalInstance.virtualTerminalInstance.kill.calledOnce).to.be.equal(true);
     });
   });
 });
