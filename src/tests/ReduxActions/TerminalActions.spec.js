69dfac4b95bf4b85ea0468aa474e72607dbf5867
Dariusz Gorak <dariusz.gorak@rspective.pl>
2018-03-12 15:32:30 +0100

    Added unit tests for new ActionCreators for Edit action

--- src/tests/ReduxActions/TerminalActions.spec.js
+++ src/tests/ReduxActions/TerminalActions.spec.js
@@ -24,6 +24,27 @@ describe('actions', () => {
     expect(TerminalActions.addNewTerminalInstance(terminalData)).to.deep.equal(expectedAction);
   });
 
+  it('should create an action to edit terminal instance', () => {
+    const expectedAction = {
+      type: TerminalActionTypes.EDIT_TERMINAL_INSTANCE,
+      terminal: {
+        terminalType: 'TERMINAL TEST TYPE',
+        terminalName: 'TEST TERMINAL',
+        terminalStartupDir: 'C:TestPath',
+        terminalStartupCommands: ['command1', 'command2'],
+      },
+    };
+
+    const terminalData = {
+      terminalType: 'TERMINAL TEST TYPE',
+      terminalName: 'TEST TERMINAL',
+      terminalStartupDir: 'C:TestPath',
+      terminalStartupCommands: ['command1', 'command2'],
+    };
+
+    expect(TerminalActions.editTerminalInstance(terminalData)).to.deep.equal(expectedAction);
+  });
+
   it('should create an action to select terminal instance', () => {
     const expectedAction = {
       type: TerminalActionTypes.SELECT_TERMINAL_INSTANCE,
---
+++ src/tests/ReduxActions/TerminalAddEditWindowActions.spec.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import AddEditTerminalWindowActionTypes from '../../app/actions/TerminalAddEditWindowActionTypes';
+import AddEditTerminalWindowAction from '../../app/actions/TerminalAddEditWindowActions';
+
+describe('actions', () => {
+  it('should create an action to show add new terminal instance window', () => {
+    const expectedAction = {
+      type: AddEditTerminalWindowActionTypes.ADD_NEW_TERMINAL_INSTANCE,
+    };
+
+    expect(AddEditTerminalWindowAction.showAddNewTerminalWindow()).to.deep.equal(expectedAction);
+  });
+
+  it('should create an action to show edit terminal instance window', () => {
+    const expectedAction = {
+      type: AddEditTerminalWindowActionTypes.EDIT_EXISTING_TERMINAL_INSTANCE,
+      terminal: {
+        uuid: '1234',
+        terminalType: 'TERMINAL TEST TYPE',
+        terminalName: 'TEST TERMINAL',
+        terminalStartupDir: 'C:TestPath',
+        terminalStartupCommands: ['command1', 'command2'],
+      },
+    };
+
+    const terminalData = {
+      uuid: '1234',
+      terminalType: 'TERMINAL TEST TYPE',
+      terminalName: 'TEST TERMINAL',
+      terminalStartupDir: 'C:TestPath',
+      terminalStartupCommands: ['command1', 'command2'],
+    };
+
+    expect(AddEditTerminalWindowAction.showEditExistingTerminalWindow(terminalData)).to.deep.equal(expectedAction);
+  });
+
+  it('should create an action to close modal window', () => {
+    const expectedAction = {
+      type: AddEditTerminalWindowActionTypes.ADD_EDIT_TERMINAL_MODAL_WINDOW_CLOSE,
+    };
+    expect(AddEditTerminalWindowAction.closeAddEditTerminalWindow()).to.deep.equal(expectedAction);
+  });
+});
