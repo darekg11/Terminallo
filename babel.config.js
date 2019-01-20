827bf3391b71cfaf8e38402c79c3139946fc6b63
Dariusz Górak <darekg11@tlen.pl>
2019-01-20 13:07:20 +0100

    Jest is running tests once more but fails for some of the tests

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
 function config(api) {
-  const presets = ["@babel/preset-env", "@babel/preset-react"];
+  const presets = ['@babel/preset-env', '@babel/preset-react'];
   const plugins = [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-transform-async-to-generator",
-    "@babel/plugin-proposal-function-bind"
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-async-to-generator',
+    '@babel/plugin-proposal-function-bind',
   ];
 
   api.cache(true);
   return {
     presets,
-    plugins
+    plugins,
   };
 }
 
