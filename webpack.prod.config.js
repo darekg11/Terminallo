4e49a6e23cfb09111d4525bb9210fbe5807f75c1
Dariusz Górak <darekg11@tlen.pl>
2019-01-20 19:46:05 +0100

    Fix production build. Enable building for Linux - unpacked and deb

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,33 +1,36 @@
-const webpack = require("webpack");
-const path = require("path");
-const UglifyJs = require("uglifyjs-webpack-plugin");
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/app/App.js"],
-  mode: "production",
-  target: "electron-main",
+  entry: ['@babel/polyfill', './src/app/App.js'],
+  mode: 'production',
+  target: 'electron-main',
 
   output: {
-    path: path.join(__dirname, "/public/built"),
-    filename: "bundle.js",
-    publicPath: "/built/"
+    path: path.join(__dirname, '/public/built'),
+    filename: 'bundle.js',
+    publicPath: '/built/',
   },
 
   module: {
     rules: [
-      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: "url-loader?limit=100000"
-      }
-    ]
+        loader: 'url-loader?limit=100000',
+      },
+    ],
   },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.ExternalsPlugin("commonjs", ["node-pty", "chokidar"]),
-    new UglifyJs()
-  ]
+    new webpack.ExternalsPlugin('commonjs', ['node-pty', 'chokidar']),
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
 };
